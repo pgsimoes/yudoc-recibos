56624d662827355d1003c160ec46bf2e0d8c9ad2
Paulo Simoes <pgsimoes@gmail.com>
2020-05-24 01:54:30 -0300

    com servidor de email

--- Gemfile
+++ Gemfile
@@ -56,7 +56,7 @@ group :test do
 end
 
 group :production do
-  gem 'pg'
+  gem 'pg', '~> 0.11'
   gem 'rails_12factor'
 end
 
--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,14 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# config.i18n.default_locale = :sv
+
+
 module YudocRecibos
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
